6934583b7c721bf2eb85b677605a237680413d00
Dennis Amparo <dennisamparo11@gmail.com>
2020-04-19 15:16:27 -0700

    Updates

---
+++ precache-manifest.da456259b9ae2c903461ec48f2c091fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899be8d8d14b9ef78d4dcfc4cdace32a",
+    "url": "/ReactPort/index.html"
+  },
+  {
+    "revision": "a5755b0d242b4b1c8002",
+    "url": "/ReactPort/static/css/main.0f07de80.chunk.css"
+  },
+  {
+    "revision": "eb8c2714e7be125ced7b",
+    "url": "/ReactPort/static/js/2.3325e777.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPort/static/js/2.3325e777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5755b0d242b4b1c8002",
+    "url": "/ReactPort/static/js/main.dae80ebf.chunk.js"
+  },
+  {
+    "revision": "03e02a3865fe224f3e8e",
+    "url": "/ReactPort/static/js/runtime-main.ebac4665.js"
+  },
+  {
+    "revision": "db0b3311d34fd2cc28314bdcff3c452e",
+    "url": "/ReactPort/static/media/BulletJournal.db0b3311.JPG"
+  },
+  {
+    "revision": "1f061730fe87d083306115606cc03d19",
+    "url": "/ReactPort/static/media/Fitness.1f061730.JPG"
+  },
+  {
+    "revision": "f07a422d748f835be7d75c17799ada5c",
+    "url": "/ReactPort/static/media/IMG_8172.f07a422d.jpg"
+  },
+  {
+    "revision": "f0dcc00fc4cdc32cde978e478201d823",
+    "url": "/ReactPort/static/media/PWgenerator.f0dcc00f.JPG"
+  },
+  {
+    "revision": "ff956892ff339b2a902d844083f3663f",
+    "url": "/ReactPort/static/media/Scheduler.ff956892.JPG"
+  },
+  {
+    "revision": "c8c732dcfd8dd7e28bb66967adc54222",
+    "url": "/ReactPort/static/media/WeatherDash.c8c732dc.JPG"
+  },
+  {
+    "revision": "bbb87b5c2d81155c0b79617770060ba7",
+    "url": "/ReactPort/static/media/mood.bbb87b5c.JPG"
+  }
+]);
